0be962f70f74d77b2f09cf63528a42d87735e3e6
何建新 <hejianxin79@sina.com>
2016-01-15 09:05:58 +0800

    asdfasdfasdfsadf

---
+++ 啊啊啊啊啊啊啊啊/啊啊啊啊啊啊啊啊/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  啊啊啊啊啊啊啊啊
+//
+//  Created by 何建新 on 16/1/15.
+//  Copyright © 2016年 何建新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 啊啊啊啊啊啊啊啊/啊啊啊啊啊啊啊啊/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  啊啊啊啊啊啊啊啊
+//
+//  Created by 何建新 on 16/1/15.
+//  Copyright © 2016年 何建新. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"asdfasdfasdfsadf");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
